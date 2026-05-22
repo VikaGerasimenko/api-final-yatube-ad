02db3f3c647d76292edbe5d8adae23a4220026ec
VikaGerasimenko <vikagerasimenko691@gmail.com>
2026-05-22 06:29:11 +0000

    Fix E128 continuation line indentation

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -19,8 +19,8 @@ router_v1.register(
 
 urlpatterns = [
     path('v1/token/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'),
+         name='token_obtain_pair'),
     path('v1/token/refresh/', TokenRefreshView.as_view(),
-        name='token_refresh'),
+         name='token_refresh'),
     path('v1/', include(router_v1.urls)),
 ]
